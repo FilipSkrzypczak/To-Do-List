08578c7d3ab5c891ff659881f8100310fbee219a
Filip <skrzypczak.filip@gmail.com>
2019-11-17 17:57:15 +0100

    Updates

---
+++ precache-manifest.0a57bbf4703add9a3193a8dfac0aa422.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "077c8a7e0e31e7542fcf55aac0102154",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "ab9782c9ff190d08d073",
+    "url": "/To-Do-List/static/css/main.3ba60cfe.chunk.css"
+  },
+  {
+    "revision": "f3d35041dcc9d4bce9a5",
+    "url": "/To-Do-List/static/js/2.3baf24e7.chunk.js"
+  },
+  {
+    "revision": "ab9782c9ff190d08d073",
+    "url": "/To-Do-List/static/js/main.3a33fddd.chunk.js"
+  },
+  {
+    "revision": "1ea607ceb0820b7c7038",
+    "url": "/To-Do-List/static/js/runtime-main.03175900.js"
+  }
+]);
